0a0eb6c9b51b3bdadf155a1d7f60a2b7af9f9113
Kaushal <lkaushal80@gmail.com>
2023-10-02 08:44:13 +0000

    request location from client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -67,4 +67,32 @@ func main() {
 		log.Fatalf("error: %s", err)
 	}
 	fmt.Println(eresp)
+
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	stream,err:=c.Location(ctx)
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
+	lreq:=pb.LocationRequest{
+		DriverId:"007",
+		Location: &pb.Location{
+			Lat:51.234234234,
+			Lng:-0.234234234,
+		},
+	}
+	for i:=0.0;i<0.03;i+=0.01{
+		lreq.Location.Lat+=i
+		if err:=stream.Send(&lreq);err!=nil{
+			log.Fatalf("error: %s",err)
+		}
+
+		
+	}
+	lresp,err:=stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
+	fmt.Println(lresp	)
+
 }
